0786e20b61e059db38ba084d89ad6529ee5b0ba0
Jasmine Chen <jasmine1226@gmail.com>
2019-08-10 21:59:23 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -15,6 +15,8 @@ class CreateCostumes << ActiveReocrd::Migration[4.2]
       t.float :price
       t.string :size
       t.string :image_url
+      t.timestamp :created_at
+      t.timestamp :updated_at
     end
   end
 end
