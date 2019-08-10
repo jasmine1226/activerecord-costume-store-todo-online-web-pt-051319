f75803f5dc95f5180f19aa4a847a68e0e22b113a
Jasmine Chen <jasmine1226@gmail.com>
2019-08-10 22:05:23 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,7 +3,13 @@
 class CreateCostumeStores < ActiveRecord::Migration[4.2]
   def change
     create_table :costume_stores do |t|
-      t.
+      t.string :name
+      t.string :location
+      t.integer :custume_inventory
+      t.integer :employees
+      t.boolean :in_business
+      t.datetime :opening_time
+      t.datetime :closing_time
     end
   end
 end
